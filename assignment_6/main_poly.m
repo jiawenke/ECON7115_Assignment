26ea02ec5e66331983b02f45bd7adfeafddfabfb
jiawenke <44312558+jiawenke@users.noreply.github.com>
2025-03-09 11:48:01 +0800

    Update main_poly.m

--- assignment_6/main_poly.m
+++ assignment_6/main_poly.m
@@ -9,13 +9,13 @@ m.alpha = 0.33;
 m.gamma = 0.5;
 m.xi = 0.2;
 m.delta = 0.07;
-% l_t?
 m.l = 1;
 m.z = 1;
 
 W0 = 1;
-m.J = 200; %#grids
+m.J = 200; % # of grids
 w_vec = (1:m.J)'/m.J*W0;
+
 %%
 a = zeros(1,5);
 diff = 1;
@@ -24,7 +24,7 @@ cc = 0;
 ww = 0.5;
 
 while diff>tol&&cc<10000
-    v_vec_func = @(w) a(1) + a(2)*(w) + a(3)*(w).^2 + a(4)*(w).^3 + a(5)*(w).^4; % 当前价值函数
+    v_vec_func = @(w) a(1) + a(2)*(w) + a(3)*(w).^2 + a(4)*(w).^3 + a(5)*(w).^4; % polynomial of Value function
     v_vec = v_vec_func(w_vec);
 
     v_vec_out = ones(m.J,1);
@@ -39,9 +39,9 @@ while diff>tol&&cc<10000
         %g_vec(j) = w_vec(rhs_vec==max(rhs_vec)); % for graph use, here only iterate Value func
     end
     
-    % How to fit a?
-    X = [ones(m.J, 1), w_vec, w_vec.^2, w_vec.^3, w_vec.^4]; % 四项式矩阵
-    a = (X' * X) \ (X' * v_vec_out); % 最小二乘法拟合
+    % Fit a using OLS
+    X = [ones(m.J, 1), w_vec, w_vec.^2, w_vec.^3, w_vec.^4]; % 4th degree polynomial
+    a = (X' * X) \ (X' * v_vec_out); % OLS approximation
     
     diff = max(abs(v_vec-v_vec_out));
     cc = cc+1;
