0a4dcab8a9b4f40b8a10c1343a01f7388894b62e
jiawenke <44312558+jiawenke@users.noreply.github.com>
2025-03-16 16:04:23 +0800

    Update and rename s_func_eqm_iter1.m to func_eqm_iter.m

--- assignment_4/Q2/func_eqm_iter.m
+++ assignment_4/Q2/func_eqm_iter.m
@@ -1,29 +1,28 @@
-function [w,X,P,L1,welfare] = s_func_eqm_iter1(tar,m)
+function [w,X,P,L,welfare] = func_eqm_iter(tarr_vec,m)
 % Solving the equilibrium outcomes by simple iteration
 
 % initial guess
 w = ones(m.R,1)/m.R;
 X = w;
 L = w;
-L1 =w;
 P = w;
 diff = 1;
 tol = 1e-6;
 ww = 0.2;
 cc = 0;
+%%
+while diff>tol && cc<1000
 
-while diff>tol && cc<100
-   [w_new,X_new,P_new,L1_new,welfare] = s_func_eqm_update1(w,X,tar,L1,P,m);
+   [w_new,X_new,P_new,L_new,welfare] = func_eqm_update(w,X,tarr_vec,L,P,m);
 
     r = [w;X;L];
-    r_new = [w_new;X_new;L1_new];
+    r_new = [w_new;X_new;L_new];
     diff = max(abs(r-r_new));
     cc = cc+1;
     
     w = ww*w_new+(1-ww)*w;
     X = ww*X_new+(1-ww)*X;
-    L1 = ww*L1_new+(1-ww)*L1;
-    %L2 = ww*L2_new+(1-ww)*L2;
+    L = ww*L_new+(1-ww)*L;
     P = ww*P_new + (1-ww)*P;
 end
 
