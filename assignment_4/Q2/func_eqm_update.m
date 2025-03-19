079856da6aa0f083f7abbdfa58b9b0fa553f7912
jiawenke <44312558+jiawenke@users.noreply.github.com>
2025-03-19 18:08:38 +0800

    Update func_eqm_update.m
    marked the code need to be improved

--- assignment_4/Q2/func_eqm_update.m
+++ assignment_4/Q2/func_eqm_update.m
@@ -4,11 +4,13 @@ function [w_new,X_new,P_new,L_new,welfare] = func_eqm_update(w,X,tarr_vec,L,P,m)
 
 
 kappa = m.tau.*(1+tarr_vec);
+% need repmat. Can't do dot multiply here
 K_mat = (w./(m.barA.*L.^m.alpha).*kappa).^(1-m.sigma);
 lambda_mat = K_mat./repmat(sum(K_mat,1),[m.R,1]); % dim1:i; dim2:n
 Xin_mat = lambda_mat.*repmat(X',[m.R,1]);
 P_new = sum(K_mat,1)'.^(1/(1-m.sigma));
 
+% should use sum(numerator,1)
 L_sum_1 = L(1)+L(2);
 L_sum_2 = L(3)+L(4);
 L_new_1 = m.barL.*(m.barB.*X./L./P).^(1./m.mu)/L_sum_1;
