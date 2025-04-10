4243f4a5d53a919f6a6ae1d9b991dae9b93c984f
jiawenke <44312558+jiawenke@users.noreply.github.com>
2025-04-10 15:25:02 +0800

    Update func_sn.m

--- Final_Project/func_sn.m
+++ Final_Project/func_sn.m
@@ -1,4 +1,3 @@
-function y = func_sn(m,w,D,X,sn)
-sn = w;
-y = sn.^(m.epsilon/m.sigma)/m.sigma*(m.sigma-1)/m.sigma.*exp((1-sn.^(m.epsilon/m.sigma))./m.epsilon).*sn.*D.*X-w.*m.F;
+function y = func_sn(m,w,D,X,s_n)
+y = (s_n.^(m.epsilon/m.sigma)./m.sigma)*((m.sigma-1)/m.sigma).*exp((1-s_n.^(m.epsilon/m.sigma))./m.epsilon).*s_n.*D.*X-w.*m.F;
 end
