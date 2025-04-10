491d0ce7076c783a7daace6ad325529f78b89447
jiawenke <44312558+jiawenke@users.noreply.github.com>
2025-04-10 15:17:00 +0800

    Update func_gen_update.m

--- Final_Project/func_gen_update.m
+++ Final_Project/func_gen_update.m
@@ -1,51 +1,55 @@
 function [w_new,X_new,M_new,P_new,D_new,welfare] = func_gen_update(w,X,M,P,D,m)
 % T_bar & lambda
-T_Bar = m.T.^m.theta./(w.*m.tau);
-M_mat = sum(M.*T_Bar,1);
-lambda_mat = (M.*T_Bar)./M_mat;
+T_Bar = repmat(m.T,[1,m.N]).^(1/m.theta)./(w.*m.tau);
+M_mat = sum(M.*T_Bar^(m.theta),1);
+lambda_mat = (M.*T_Bar^(m.theta))./M_mat;
 
 % Generate random J from uniform distribution
 rng(123); % set seed
-u = sort(rand(m.J, 2));
+u = sort(rand(m.J,2));
 
-%initial guess should be CES results
-sn = w;
+s_n = w;
 options = optimoptions('fsolve','Display','iter','MaxFunEvals',1e6,'MaxIter',1000,'TolFun',1e-10,'TolX',1e-10);
-[sn] = fsolve(@(sn)func_sn(m,w,D,sn),sn,options);
-sn_star = sn;
+[s_n] = fsolve(@(s_n)func_sn(m,w,D,X,s_n),s_n,options);
+sn_star = s_n;
 
-c_star = (m.sigma-sn_star.^(m.epsilon/m.sigma))./m.sigma*(m.sigma-1)./m.sigma.*exp((1-sn_star.^(m.epsilon/m.sigma))./m.epsilon).*sn_star.*D.*P;
+c_star = ((m.sigma-sn_star.^(m.epsilon/m.sigma))./m.sigma)*((m.sigma-1)./m.sigma).*exp((1-sn_star.^(m.epsilon/m.sigma))./m.epsilon).*D.*P;
 c_nj = u.*c_star'; % c_n^j = u_j * c_star
 
 s_j = u;
 options = optimoptions('fsolve','Display','iter','MaxFunEvals',1e6,'MaxIter',1000,'TolFun',1e-10,'TolX',1e-10);
 [s_j] = fsolve(@(s_j)func_sj(m,P,D,s_j,c_nj),s_j,options);
 s_nj = s_j;
+disp(s_nj)
 
-% compute yita ? 虚数,可能是由于initial guess引起了sj是负数，导致这里出现虚数
-yita_mat = s_nj.^(m.epsilon/m.sigma).*exp((1-s_nj.^(m.epsilon/m.sigma))./m.epsilon).*s_nj.*c_nj.^(m.theta-1);
-yita = sum(yita_mat,1);
+% compute yita 
+yita_mat_1 = s_nj.^(m.epsilon/m.sigma).*exp((1-s_nj.^(m.epsilon/m.sigma))./m.epsilon).*s_nj.*c_nj.^(m.theta-1);
+yita_mat_2 = exp((1-s_nj.^(m.epsilon/m.sigma))./m.epsilon).*s_nj.*c_nj.^(m.theta-1);
+yita = sum(yita_mat_1,1)./sum(yita_mat_2,1);
 
-% compute nu_P 虚数 same reason
-nu_mat = m.theta*m.sigma./(m.sigma-s_nj.^(m.epsilon/m.theta)).*s_nj.*c_nj.^(m.theta);
-nu_P = c_star./m.J*sum(nu_mat,1);
+% compute nu_P
+nu_mat = m.theta*m.sigma./(m.sigma-s_nj.^(m.epsilon/m.sigma)).*s_nj.*c_nj.^(m.theta);
+nu_P = (c_star'./m.J).*sum(nu_mat,1);
 
-% update Dn ? H(x) gammainc function
-MT_mat = M.*T_Bar.^m.theta;
-H_mat = func_hx(m,s_nj).*c_nj^(m.theta-1);
-D_new = D.*(MT_mat.*c_star./m.J*m.theta*sum(H_mat,2))^(1/(1+m.theta));
+% update Dn ? which dim is i ? why c_nj^(theta-1)=0?
+MT = M.*T_Bar.^m.theta;
+MT_mat = sum(MT,1);
+H_mat = func_hx(m,s_nj).*c_nj.^(m.theta-1);
+D_new = D.*((MT_mat'.*c_star./m.J*m.theta).*sum(H_mat,1)').^(1/(1+m.theta));
 
-% update w_i
-prod = (1-yita).*lambda_mat.*X;
-fmkt_cost = w.*m.F.*c_star.^m.theta*sum(MT_mat,1);
-net_prof = yita.*lambda_mat.*X-w.*m.F.*c_star.^m.theta*M.*T_Bar^m.theta;
+% update w_i, net prof is negative? c_star too small and theta too large
+prod_mat = (1-yita).*lambda_mat.*X;
+prod = sum(prod_mat,2);
+fmkt_cost = w.*m.F.*c_star.^m.theta.*MT_mat';
+net_prof_mat = yita.*lambda_mat.*X-w.*m.F.*(c_star.^m.theta).*MT;
+net_prof = sum(net_prof_mat,2);
 w_new = prod + fmkt_cost + net_prof;
 
-% update M_i, what is f^e? f^e=1
-M_new = net_prof./w./f;
+% update M_i
+M_new = net_prof./w./m.f;
 
 % update P_n
-P_new = (P^(-m.theta-1)*nu_P.*MT_mat)^(-1/m.theta);
+P_new = (P.^(-m.theta-1).*nu_P'.*MT_mat').^(-1/m.theta);
 
 % update X_n, do we need to update X_n?
 X_new = w.*m.L;
@@ -56,6 +60,6 @@ welfare = w./P;
 wnum = sum(w_new);
 w_new = w_new/wnum;
 X_new = X_new/wnum;
-P_new = P_new./wnum;
+% P_new = P_new./wnum;
 
 end
