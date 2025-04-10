83b632dc04317614af9c7fbbbb64588f42a9ee44
jiawenke <44312558+jiawenke@users.noreply.github.com>
2025-04-10 15:24:07 +0800

    Update func_hx.m

--- Final_Project/func_hx.m
+++ Final_Project/func_hx.m
@@ -1,13 +1,11 @@
-function y = func_hx(m,sn)
+function y = func_hx(m,s_nj)
 a = m.sigma / m.epsilon;
 x1 = 1 / m.epsilon;
-gamma_1 = gamma(a) * (1 - gammainc(x1, a, 'upper'));
+gamma_1 = gammainc(a,x1,'upper');
 
-% 计算 Gamma(sigma/epsilon, x^(theta)/epsilon)
-x2 = sn.^(m.epsilon) / m.sigma; 
-gamma_2 = gamma(a) * (1 - gammainc(x2, a, 'upper'));
+x2 = s_nj.^(m.epsilon/m.sigma)/m.epsilon;
+gamma_2 = gammainc(a,x2,'upper');
 
-% 计算 H(x)
-y = 1 + (sigma - 1) * exp(1/epsilon) * epsilon^((sigma-1)/epsilon) * ...
-         (gamma_1 - gamma_2);
+
+y = 1 + (m.sigma - 1) * exp(1/m.epsilon) * m.epsilon^((m.sigma/m.epsilon)-1) *(gamma_1 - gamma_2);
 end
